6445317065f0bcec737595826ef713ab11cd51b8
Dan Stevens <danstevens@users.noreply.github.com>
2022-01-14 14:02:24 +0000

    Modified BabelNetApiClient.cs:  - Added overloads for GetSynsetIdsAsync method  - Added `RequestLog` and `ResponseLog` properties, which tests can supply with lists and later read for verification
    Modified BabelNetApiClientTests.cs:
     - Added tests for GetSyncSetIds method

--- BabelNet.HttpApi.Tests/BabelNetApiClientTests.cs
+++ BabelNet.HttpApi.Tests/BabelNetApiClientTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace BabelNet.HttpApi.Tests;
 
@@ -8,17 +10,85 @@ public class BabelNetApiClientTests
 {
     private const string _apiKey = Secrets.ApiKey;
 
+    private HttpClient httpClient;
+    private BabelNetApiClient apiClient;
+
     [SetUp]
     public void Setup()
     {
+        httpClient = new HttpClient();
+        apiClient = new(httpClient, _apiKey)
+        {
+            RequestLog = new List<HttpRequestMessage>(),
+            ResponseLog = new List<HttpResponseMessage>(),
+        };
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        httpClient.Dispose();
     }
 
     [Test]
     public async Task GetVersion()
     {
-        var babelNetApiClient = new BabelNetApiClient(new HttpClient(), _apiKey);
-        var res = await babelNetApiClient.GetVersionAsync();
+        var res = await apiClient.GetVersionAsync();
+
+        apiClient.RequestLog.Count.Should().Be(1);
+        apiClient.RequestLog[0].Method.Should().Be(HttpMethod.Get);
+        apiClient.RequestLog[0].RequestUri.Should().Be(
+            $"https://babelnet.io/v6/getVersion?key={_apiKey}");
+
+        res.Should().NotBeNull();
+        res.Version.Should().Be("V5_0");
+    }
+
+    [Test]
+    public async Task GetSynsetIds_WithArgs_lemma_searchLang()
+    {
+        const string lemma = "apple";
+        const string searchLang = "EN";
+        var res = await apiClient.GetSynsetIdsAsync(lemma, searchLang);
+
+        apiClient.RequestLog.Count.Should().Be(1);
+        apiClient.RequestLog[0].Method.Should().Be(HttpMethod.Get);
+        apiClient.RequestLog[0].RequestUri.Should().Be(
+            $"https://babelnet.io/v6/getSynsetIds?lemma={lemma}&searchLang={searchLang}&key={_apiKey}");
+
         Assert.IsNotNull(res);
-        Assert.AreEqual("V5_0", res.Version);
+    }
+
+    [Test]
+    public async Task GetSynsetIds_WithArgs_lemma_searchLangs()
+    {
+        const string lemma = "apple";
+        var searchLangs = new[] { "EN", "DE", "ES" };
+        var res = await apiClient.GetSynsetIdsAsync(lemma, searchLangs);
+
+        apiClient.RequestLog.Count.Should().Be(1);
+        apiClient.RequestLog[0].Method.Should().Be(HttpMethod.Get);
+        apiClient.RequestLog[0].RequestUri.Should().Be(
+            $"https://babelnet.io/v6/getSynsetIds?lemma={lemma}&searchLang={string.Join("&searchLang=", searchLangs)}&key={_apiKey}");
+
+        Assert.IsNotNull(res);
+    }
+
+    [Test]
+    public async Task GetSyncSetIds_WithArgs_lemma_searchLang_targetLang_pos_source()
+    {
+        const string lemma = "apple";
+        const string searchLang = "EN";
+        const string targetLang = "DE";
+        const UniversalPOS pos = UniversalPOS.NOUN;
+        const string source = "BABELNET";
+        var res = await apiClient.GetSynsetIdsAsync(lemma, searchLang, targetLang, pos, "BABELNET");
+
+        apiClient.RequestLog.Count.Should().Be(1);
+        apiClient.RequestLog[0].Method.Should().Be(HttpMethod.Get);
+        apiClient.RequestLog[0].RequestUri.Should().Be(
+            $"https://babelnet.io/v6/getSynsetIds?lemma={lemma}&searchLang={searchLang}&targetLang={targetLang}&pos={pos}&source={source}&key={_apiKey}");
+
+        Assert.IsNotNull(res);
     }
 }
--- BabelNet.HttpApi/BabelNetApiClient.cs
+++ BabelNet.HttpApi/BabelNetApiClient.cs
@@ -13,17 +13,76 @@ public partial class BabelNetApiClient
         _apiKey = apiKey;
     }
 
+    /// <summary>
+    /// Optionally set to a list in order to log request messages
+    /// </summary>
+    public IList<HttpRequestMessage> RequestLog { get; set; }
+
+    /// <summary>
+    /// Optionally set to a list in order to log response messages
+    /// </summary>
+    public IList<HttpResponseMessage> ResponseLog { get; set; }
+
     partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
     {
         if (!string.IsNullOrWhiteSpace(_apiKey))
         {
             // Append `key` query parameter to each request with _apiKey
             Uri uri = new Uri(url);
-            string newQuery = string.Format("{0}{1}key={2}",
-                uri.Query,
-                string.IsNullOrWhiteSpace(uri.Query) ? "?" : "&",
-                Uri.EscapeDataString(_apiKey));
-            request.RequestUri = new Uri(uri.ToString() + newQuery);
+            request.RequestUri = new Uri(uri.ToString() + GetKeyQueryParameter(uri));
         }
+
+        if (RequestLog != null)
+        {
+            RequestLog.Add(request);
+        }
+
+        string GetKeyQueryParameter(Uri uri)
+        {
+            return (string.IsNullOrWhiteSpace(uri.Query) ? "?" : "&") + "key=" + _apiKey;
+        }
+    }
+
+    partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
+    {
+        if (ResponseLog != null)
+        {
+            ResponseLog.Add(response);
+        }
+    }
+
+    public Task<ICollection<Synset>> GetSynsetIdsAsync(string lemma, string searchLang)
+    {
+        return GetSynsetIdsAsync(
+            lemma,
+            new[] { searchLang },
+            null,
+            null,
+            null);
+    }
+
+    public Task<ICollection<Synset>> GetSynsetIdsAsync(string lemma, IEnumerable<string> searchLangs)
+    {
+        return GetSynsetIdsAsync(
+            lemma,
+            searchLangs,
+            null,
+            null,
+            null);
+    }
+
+    public Task<ICollection<Synset>> GetSynsetIdsAsync(
+        string lemma,
+        string searchLang,
+        string? targetLang = null,
+        UniversalPOS? pos = null,
+        string? source = null)
+    {
+        return GetSynsetIdsAsync(
+            lemma,
+            new[] { searchLang },
+            new[] { targetLang },
+            pos,
+            source);
     }
 }
