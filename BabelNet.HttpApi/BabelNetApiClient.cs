abf7033bd463c8d12c5bc7f0d301c25e4cdefab6
Dan Stevens <danstevens@users.noreply.github.com>
2022-01-20 17:10:44 +0000

    Downgraded from .NET 6.0 to .NET 5.0 for greater compatability

--- BabelNet.HttpApi/BabelNetApiClient.cs
+++ BabelNet.HttpApi/BabelNetApiClient.cs
@@ -1,79 +1,84 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BabelNet.HttpApi;
-
-public partial class BabelNetApiClient
+namespace BabelNet.HttpApi
 {
-
-    private string _apiKey = null;
-
-    public BabelNetApiClient(HttpClient httpClient, string apiKey)
-        : this(httpClient)
+    public partial class BabelNetApiClient
     {
-        _apiKey = apiKey;
-    }
 
+        private string _apiKey = null;
 
-    partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
-    {
-        if (!string.IsNullOrWhiteSpace(_apiKey))
+        public BabelNetApiClient(HttpClient httpClient, string apiKey)
+            : this(httpClient)
         {
-            // Append `key` query parameter to each request with _apiKey
-            url = url + GetKeyQueryParameter(new Uri(url));
-            request.RequestUri = new Uri(url);
+            _apiKey = apiKey;
         }
 
-        OnRequesting(client, request, url);
 
-        string GetKeyQueryParameter(Uri uri)
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
         {
-            return (string.IsNullOrWhiteSpace(uri.Query) ? "?" : "&") + "key=" + _apiKey;
+            if (!string.IsNullOrWhiteSpace(_apiKey))
+            {
+                // Append `key` query parameter to each request with _apiKey
+                url = url + GetKeyQueryParameter(new Uri(url));
+                request.RequestUri = new Uri(url);
+            }
+
+            OnRequesting(client, request, url);
+
+            string GetKeyQueryParameter(Uri uri)
+            {
+                return (string.IsNullOrWhiteSpace(uri.Query) ? "?" : "&") + "key=" + _apiKey;
+            }
         }
+
+        partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
+        {
+            OnResponse(client, response);
+        }
+
+        public Task<ICollection<SynsetId>> GetSynsetIdsAsync(string lemma, string searchLang)
+        {
+            return GetSynsetIdsAsync(
+                lemma,
+                new[] { searchLang },
+                null,
+                null,
+                null);
+        }
+
+        public Task<ICollection<SynsetId>> GetSynsetIdsAsync(string lemma, IEnumerable<string> searchLangs)
+        {
+            return GetSynsetIdsAsync(
+                lemma,
+                searchLangs,
+                null,
+                null,
+                null);
+        }
+
+        public Task<ICollection<SynsetId>> GetSynsetIdsAsync(
+            string lemma,
+            string searchLang,
+            string? targetLang = null,
+            UniversalPOS? pos = null,
+            string? source = null)
+        {
+            return GetSynsetIdsAsync(
+                lemma,
+                new[] { searchLang },
+                new[] { targetLang },
+                pos,
+                source);
+        }
+
+        protected virtual void OnRequesting(HttpClient client, HttpRequestMessage request, string url)
+        { }
+
+        protected virtual void OnResponse(HttpClient client, HttpResponseMessage response)
+        { }
     }
-
-    partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
-    {
-        OnResponse(client, response);
-    }
-
-    public Task<ICollection<SynsetId>> GetSynsetIdsAsync(string lemma, string searchLang)
-    {
-        return GetSynsetIdsAsync(
-            lemma,
-            new[] { searchLang },
-            null,
-            null,
-            null);
-    }
-
-    public Task<ICollection<SynsetId>> GetSynsetIdsAsync(string lemma, IEnumerable<string> searchLangs)
-    {
-        return GetSynsetIdsAsync(
-            lemma,
-            searchLangs,
-            null,
-            null,
-            null);
-    }
-
-    public Task<ICollection<SynsetId>> GetSynsetIdsAsync(
-        string lemma,
-        string searchLang,
-        string? targetLang = null,
-        UniversalPOS? pos = null,
-        string? source = null)
-    {
-        return GetSynsetIdsAsync(
-            lemma,
-            new[] { searchLang },
-            new[] { targetLang },
-            pos,
-            source);
-    }
-
-    protected virtual void OnRequesting(HttpClient client, HttpRequestMessage request, string url)
-    { }
-
-    protected virtual void OnResponse(HttpClient client, HttpResponseMessage response)
-    { }
 }
